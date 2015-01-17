6cd0c3837afda234bcab961dbb84dbdca96de57e
Louis Pilfold <louis@lpil.uk>
2015-01-17 17:01:34 +0000

    Move note functions into theory namespace

--- src/lans_syn/core.clj
+++ src/lans_syn/core.clj
@@ -3,26 +3,7 @@
 ; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 (ns lans-syn.core
-  (:require [clojure.math.numeric-tower :as tow]
+  (:require [lans-syn.theory :as theory]
             [overtone.live :as o]))
 
-(defn oscs
-  "Set up the oscillators"
-  []
-  (prn (take 720 (range))))
 
-(defn TET-N-note->freq
-  "Converts a TET N note number to it's frequency, where N is the
-  number of semitones per octave in that scale.
-  Reference pitch is A4: 440 hertz"
-  [note semitones-per-octave]
-  (let [bottom-freq 16.352
-        TET-freq-ratio (tow/expt 2 (/ 1 semitones-per-octave))]
-    (* bottom-freq
-       (tow/expt TET-freq-ratio note))))
-
-(defn TET72-note->freq
-  "Converts a note number to it's frequency for TET72.
-  Reference pitch is 440 hertz"
-  [note]
-  (TET-N-note->freq note 72))
---
+++ src/lans_syn/theory.clj
@@ -0,0 +1,22 @@
+; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this
+; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+(ns lans-syn.theory
+  (:require [clojure.math.numeric-tower :as tow]))
+
+(defn TET-N-note->freq
+  "Converts a TET N note number to it's frequency, where N is the
+  number of semitones per octave in that scale.
+  Reference pitch is A4: 440 hertz"
+  [note semitones-per-octave]
+  (let [bottom-freq 16.352
+        TET-freq-ratio (tow/expt 2 (/ 1 semitones-per-octave))]
+    (* bottom-freq
+       (tow/expt TET-freq-ratio note))))
+
+(defn TET72-note->freq
+  "Converts a note number to it's frequency for TET72.
+  Reference pitch is 440 hertz"
+  [note]
+  (TET-N-note->freq note 72))
